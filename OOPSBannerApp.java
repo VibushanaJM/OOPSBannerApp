f77c0f5217d39c22394f200f11b6afc183360c9a
VibushanaJM <vm7102@srmist.edu.in>
2026-03-03 14:51:36 +0530

    Printing OOPS Banner

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,52 +1,20 @@
 /**
  * @author [VIBUSHANA]
- * @version 1.0
+ * @version 1.0 UC2-Print OOPS Banner
  */
 public class OOPSBannerApp {
-    public static String[] getOPattern(){
-        return new String[]{
-            "   ***   ",
-            "**     **",
-            "**     **",
-            "**     **",
-            "   ***   ",
-        };
-    }
-    public static String[] getO2Pattern(){
-        return new String[]{
-            "   ***   ",
-            "**     **",
-            "**     **",
-            "**     **",
-            "   ***   ",
-        };
-    }
-    public static String[] getPPattern(){
-        return new String[]{
-            "******",
-            "**    *",
-            "*     * ",
-            "****** ",
-            "**      ",
-            "**      ",
-        };
-    }  
-    public static String[] getSPattern(){
-        return new String[]{
-            "   ***** ",
-            " **      ",
-            "  *****   ",
-            "       ** ",
-            "  *****   ",
-        };
-    }
+
     public static void main(String[] args) {
-        String[] oPattern = getOPattern();
-        String[] o2Pattern = getO2Pattern();
-        String[] pPattern = getPPattern();
-        String[] sPattern = getSPattern();
-        for (int i = 0; i < oPattern.length; i++) {
-            System.out.println(oPattern[i] + " " + o2Pattern[i] + " " +  pPattern[i] + " " + sPattern[i]);
-        }
-   };
+        // Now we just call the method instead of writing all the print lines here
+        displayOopsBanner();
+    }
+
+    // This is the new function (method) we created to hold the banner logic
+    public static void displayOopsBanner() {
+        System.out.println(" ****   ****  ****  **** ");
+        System.out.println("*    * *    * *   * * ");
+        System.out.println("*    * *    * ****  **** ");
+        System.out.println("*    * *    * *        *");
+        System.out.println(" ****   ****  *     **** ");
+    }
 }
